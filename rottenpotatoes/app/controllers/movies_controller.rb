db92bed45bfb988f93285fc47e01f462bb5f49ee
EC2 Default User <ec2-user@ip-172-31-37-216.us-east-2.compute.internal>
2021-03-05 01:31:52 +0000

    update

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,26 +7,85 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      ordering,@title_header = {:title => :asc}, 'bg-warning hilite'
-    when 'release_date'
-      ordering,@date_header = {:release_date => :asc}, 'bg-warning hilite'
-    end
+  if request.env['PATH_INFO']== '/'
+    session.clear
+  end
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-
-    if @selected_ratings == {}
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+#    @filtered_ratings=params[:ratings]
+    
+#    @Keys=( params[:ratings].nil? ? [] : @filtered_ratings.keys)
+ #   @movies=@filtered_ratings==[] ? Movie.all: Movie.with_ratings(@Keys)
+    
+ #   @ratings_to_show=@Keys
+    
+    @flag=0
+    @flag2=0
+  #  print('*-------')
+  #  print(params[:utf8])
+  #  print('-------')
+    
+    
+    if params[:ratings] 
+      @filtered_ratings = params[:ratings]
+      session[:ratings] = @filtered_ratings
+    elsif params[:click_button]
+      @filtered_ratings = nil
+      #If user selects no filter then everything should get cleared
+      session[:sort_by] =nil
+    
+      session[:ratings] =nil
+    elsif session[:ratings] 
+      flag=1
+      @filtered_ratings = session[:ratings]
+    else
+      @filtered_ratings = nil
     end
 
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    if params[:sort_by]
+      @sort_order = params[:sort_by]
+      session[:sort_by] = @sort_order
+    elsif session[:sort_by]
+      flag=1
+      @sort_order = session[:sort_by]
+    else
+      @sort_order = nil
+    end 
+    if flag==1
+      redirect_to movies_path :sort_by => @sort_order, :ratings => @filtered_ratings
     end
-    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+    
+    @ratings_to_show=[]
+    if @filtered_ratings
+      @ratings_to_show=@filtered_ratings.keys
+    end
+    # I removed this part of code which used to put back filters once sort is added 
+    # if !@filtered_ratings
+    #   @filtered_ratings = @all_ratings.each_with_object('1').to_h
+    
+    # end    
+    
+    if @sort_order and @filtered_ratings
+      @movies = Movie.fliter_ratings_order(@filtered_ratings.keys,@sort_order)
+   #   @ratings_to_show=@filtered_ratings.keys
+    elsif @filtered_ratings
+      @movies = Movie.fliter_ratings(@filtered_ratings.keys)
+  #    @ratings_to_show=@filtered_ratings.keys
+    elsif @sort_order
+      @movies = Movie.all.order(@sort_order)
+    else
+      @movies = Movie.all
+    end   
+    
+  #   if(params[:sort_by]=='title')
+   #   @movies=Movie.order:title
+#    end
+ #   if(params[:sort_by]=='date')
+  #    @movies=Movie.order:release_date
+   # end
+    
+  
+   
+   
   end
 
   def new
@@ -34,7 +93,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -45,7 +104,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -56,10 +115,11 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
-  private
-    def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
-    end
 
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 end
